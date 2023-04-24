28b5318cb96b0ee80baba503d1cb0dfbc16106c1
Pontus Tengroth <pontus.tengroth@leija.se>
2023-04-24 10:05:29 +0200

    file

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const {clipboard} = require('electron')
-const EventEmitter = require('./EventEmitter')
-const clipboardEmitter = new EventEmitter()
+const { clipboard } = require("electron");
+const EventEmitter = require("./EventEmitter");
+const { spawnSync } = require("child_process");
+const clipboardEmitter = new EventEmitter();
 
-let watcherId = null, previousText = clipboard.readText(), previousImage = clipboard.readImage()
+const getFileClip = () =>
+  spawnSync("powershell", [
+    "-Command",
+    "Get-Clipboard -Format FileDropList",
+  ]).stdout.toString()
+
+let watcherId = null, previousText = clipboard.readText(), previousImage = clipboard.readImage(), previousFile = getFileClip();
 
 clipboard.on = (event, listener) => {
     clipboardEmitter.on(event, listener)
@@ -23,12 +30,23 @@ clipboard.off = (event, listener) => {
 }
 
 clipboard.startWatching = () => {
-    if(!watcherId)
-        watcherId = setInterval(() => {
-            if(isDiffText(previousText, previousText = clipboard.readText())) clipboardEmitter.emit('text-changed')
-            if(isDiffImage(previousImage, previousImage = clipboard.readImage())) clipboardEmitter.emit('image-changed')
-        }, 500)
-    return clipboard
+  if (!watcherId)
+    watcherId = setInterval(() => {
+      if (process.platform == "win32") {
+        if (isDiffText(previousText, (previousText = clipboard.readText())))
+          clipboardEmitter.emit("text-changed");
+        if (isDiffImage(previousImage, (previousImage = clipboard.readImage())))
+          clipboardEmitter.emit("image-changed");
+        if (isDiffFile(previousFile, (previousFile = getFileClip())))
+          clipboardEmitter.emit("file-changed");
+      } else {
+        if (isDiffText(previousText, (previousText = clipboard.readText())))
+          clipboardEmitter.emit("text-changed");
+        if (isDiffImage(previousImage, (previousImage = clipboard.readImage())))
+          clipboardEmitter.emit("image-changed");
+      }
+    }, 500);
+  return clipboard;
 }
 
 clipboard.stopWatching = () => {
@@ -43,6 +61,10 @@ function isDiffText(str1, str2){
 
 function isDiffImage(img1, img2){
     return !img2.isEmpty() && img1.toDataURL() !== img2.toDataURL()
+}
+
+function isDiffFile(file1, file2) {
+  return file2 && file1 !== file2;
 }
 
 module.exports = clipboard
