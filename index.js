783c56d7005f4f4e5ffcd7a12660920b791512d7
AsmatxD <95101672+AsmatxD@users.noreply.github.com>
2023-04-26 21:40:03 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,70 +1,65 @@
 const { clipboard } = require("electron");
 const EventEmitter = require("./EventEmitter");
-const { spawnSync } = require("child_process");
+const { PowerShell } = require("node-powershell");
 const clipboardEmitter = new EventEmitter();
 
-const getFileClip = () =>
-  spawnSync("powershell", [
-    "-Command",
-    "Get-Clipboard -Format FileDropList",
-  ]).stdout.toString()
+/* Get-Clipboard -Format FileDropList -Raw
 
-let watcherId = null, previousText = clipboard.readText(), previousImage = clipboard.readImage(), previousFile = getFileClip();
+Set-Clipboard -Path 'H:\My Documents\linux-basics-1-2-exercises-swedish.pdf' [-Append] */
+
+const getFileClip = () =>
+  PowerShell.$("Get-Clipboard -Format FileDropList -Raw");
+
+let watcherId = null,
+  previousText = clipboard.readText(),
+  previousImage = clipboard.readImage(),
+  previousFile = getFileClip();
 
 clipboard.on = (event, listener) => {
-    clipboardEmitter.on(event, listener)
-    return clipboard
-}
+  clipboardEmitter.on(event, listener);
+  return clipboard;
+};
 
 clipboard.once = (event, listener) => {
-    clipboardEmitter.once(event, listener)
-    return clipboard
-}
+  clipboardEmitter.once(event, listener);
+  return clipboard;
+};
 
 clipboard.off = (event, listener) => {
-    if(listener)
-        clipboardEmitter.removeListener(event, listener)
-    else
-        clipboardEmitter.removeAllListeners(event)
-    return clipboard
-}
+  if (listener) clipboardEmitter.removeListener(event, listener);
+  else clipboardEmitter.removeAllListeners(event);
+  return clipboard;
+};
 
 clipboard.startWatching = () => {
   if (!watcherId)
     watcherId = setInterval(() => {
-      if (process.platform == "win32") {
-        if (isDiffText(previousText, (previousText = clipboard.readText())))
-          clipboardEmitter.emit("text-changed");
-        if (isDiffImage(previousImage, (previousImage = clipboard.readImage())))
-          clipboardEmitter.emit("image-changed");
-        if (isDiffFile(previousFile, (previousFile = getFileClip())))
-          clipboardEmitter.emit("file-changed");
-      } else {
-        if (isDiffText(previousText, (previousText = clipboard.readText())))
-          clipboardEmitter.emit("text-changed");
-        if (isDiffImage(previousImage, (previousImage = clipboard.readImage())))
-          clipboardEmitter.emit("image-changed");
-      }
+      if (isDiffText(previousText, (previousText = clipboard.readText())))
+        clipboardEmitter.emit("text-changed");
+      if (isDiffImage(previousImage, (previousImage = clipboard.readImage())))
+        clipboardEmitter.emit("image-changed");
+      if (isDiffFile(previousFile, (previousFile = getFileClip())))
+        clipboardEmitter.emit("file-changed");
     }, 500);
   return clipboard;
-}
+};
 
 clipboard.stopWatching = () => {
-    if(watcherId) clearInterval(watcherId)
-    watcherId = null
-    return clipboard
+  if (watcherId) clearInterval(watcherId);
+  watcherId = null;
+  return clipboard;
+};
+
+function isDiffText(str1, str2) {
+  return str2 && str1 !== str2;
 }
 
-function isDiffText(str1, str2){
-    return str2 && str1 !== str2
-}
-
-function isDiffImage(img1, img2){
-    return !img2.isEmpty() && img1.toDataURL() !== img2.toDataURL()
+function isDiffImage(img1, img2) {
+  return !img2.isEmpty() && img1.toDataURL() !== img2.toDataURL();
 }
 
 function isDiffFile(file1, file2) {
   return file2 && file1 !== file2;
 }
 
-module.exports = clipboard
+module.exports = clipboard;
